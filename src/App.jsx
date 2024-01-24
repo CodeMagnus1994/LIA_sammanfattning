3ad21ae98553277b85a2c315ac69b5cac606e7da
CodeMagnus1994 <magnusolsson1994@hotmail.se>
2024-01-24 09:32:39 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import GetTodo from './components/getTodo'
-
+import React from 'react';
+import GetTodo from './components/getTodo';
+import Message from './components/message';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
@@ -16,10 +17,12 @@ function App() {
   return (
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialsOpen={false}/>
-        <div>
-          Hello Still working!
-        </div>
-        <GetTodo/>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/gettodo" element={<GetTodo/>}/>
+        <Route path="/message" element={<Message/>}/>
+        </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
   )
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function message() {
+  return (
+    <div>
+        Hello from Message.jsx
+    </div>
+  )
+}
+
+export default message
